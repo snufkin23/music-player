4c35bcd54b803f44abdcf61f0e25d8a96bc7aa3f
Sushant <sushantkhadka23@proton.me>
2026-08-13 20:36:35 +0545

    feat: integrate PermissionGate and update MainActivity to display LibraryScreen

--- app/src/main/java/com/sushantkhadka/musicplayer/MainActivity.kt
+++ app/src/main/java/com/sushantkhadka/musicplayer/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sushantkhadka.musicplayer.permission.PermissionGate
 import com.sushantkhadka.musicplayer.ui.theme.MusicplayerTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,11 +22,8 @@ class MainActivity : ComponentActivity() {
 		enableEdgeToEdge()
 		setContent {
 			MusicplayerTheme {
-				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Greeting(
-						name = "Android",
-						modifier = Modifier.padding(innerPadding)
-					)
+				PermissionGate {
+					com.sushantkhadka.musicplayer.ui.library.LibraryScreen()
 				}
 			}
 		}
