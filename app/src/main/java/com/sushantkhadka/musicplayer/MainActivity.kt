216d896492994bbe8eccdf6580a03fbea81ca36a
Sushant <sushantkhadka23@proton.me>
2026-08-13 23:57:53 +0545

    refactor: remove unused Greeting composable and related imports from MainActivity

--- app/src/main/java/com/sushantkhadka/musicplayer/MainActivity.kt
+++ app/src/main/java/com/sushantkhadka/musicplayer/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.sushantkhadka.musicplayer.permission.PermissionGate
 import com.sushantkhadka.musicplayer.ui.theme.MusicplayerTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,21 +20,5 @@ class MainActivity : ComponentActivity() {
 				}
 			}
 		}
-	}
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-	MusicplayerTheme {
-		Greeting("Android")
 	}
 }
