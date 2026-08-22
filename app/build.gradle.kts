eca5d02969bba28ab523a8e0a7eef9c336a4c9fd
Sushant <sushantkhadka23@proton.me>
2026-08-22 21:40:20 +0545

    feat: add Navigation Compose dependency to build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
 	implementation(libs.androidx.media3.exoplayer)
 	implementation(libs.androidx.media3.session)
 
+	implementation(libs.androidx.navigation.compose)
+
 	implementation(libs.androidx.compose.material.icons.extended)
 
 	implementation(platform(libs.androidx.compose.bom))
